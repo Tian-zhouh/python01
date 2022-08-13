df5cd8032e5e09a0b335c65d770f94908a914d03
zhoutonghao <1789714191@qq.com>
2022-08-13 13:58:55 +0800

    # del

---
+++ python001/day02/day01作业.py
@@ -0,0 +1,11 @@
+# 排错题
+
+name = input("请输入姓名:")
+age = input("请输入姓名:")
+print("输入的是:" + name)
+print("年龄是:" + age)
+
+# 内存图题目
+count = 100
+sum = count + 5
+count = 0
---
+++ python001/day02/del.py
@@ -0,0 +1,9 @@
+# del 语句
+# del 变量名
+
+a = "清醒啊"
+b = a
+
+print(a)
+del a
+print(b)
