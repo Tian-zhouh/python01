f59107658f00828439903ceec485e251b114cb5a
zhoutonghao <1789714191@qq.com>
2022-08-14 08:40:04 +0800

    #位置调整

--- python001/day02/数据类型/练习.py
+++ python001/day02/数据类型/练习.py
@@ -4,4 +4,5 @@ age = int(input("age>>>"))
 sex = input("sex>>>")
 score = float(input("score>>>"))
 
+# 在固定格式的字符串中，插入不同类型的对象
 print("我的姓名是:" + name + ",年龄是:" + str(age) + ",性别是:" + sex + ",成绩是:" + str(score))
---
+++ python001/day02/运算符/算数运算符.py
@@ -0,0 +1,32 @@
+"""
+算数运算符
+"""
+
+num01 = 5
+num02 = 2
+print(num01 / num02)
+print(num01 // num02)
+print(num01 % num02)
+
+# 取余的作用
+# 1.判断一个数能否被另外一个数整除
+print(num01 % 5 == 0)
+print(num01 % 2 == 0)
+
+# 2.获取个位
+print(67 % 10)
+
+# 注意数据类型不统一的问题
+
+# 比较运算符：略
+# 返回布尔值
+
+
+# 增强运算符
+num03 = 1
+print(num03 + 1)
+print(num03)
+
+num04 = 1
+num04 += 1
+print(num04)
