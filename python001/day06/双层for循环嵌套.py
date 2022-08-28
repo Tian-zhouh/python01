6df35efc0fde0f7d225bd42616a2d72c5c1dd96a
zhoutonghao <1789714191@qq.com>
2022-08-28 20:01:28 +0800

    #双层for循环

--- python001/day06/双层for循环嵌套.py
+++ python001/day06/双层for循环嵌套.py
@@ -11,3 +11,28 @@ for i in range(3):
     for j in range(5):
         print("*", end="")
     print()
+
+"""
+在控制台输出
+******
+######
+******
+######
+"""
+for i in range(2):
+    for j in range(6):
+        print("*", end="")
+    print()
+    for j in range(6):
+        print("#", end="")
+    print()
+
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+for i in range(4):
+    for j in range(6):
+        if i % 2 == 0:
+            print("*", end="")
+        else:
+            print("#", end="")
+    print()
+34: 34
