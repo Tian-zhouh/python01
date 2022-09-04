4d106d6d63f7908eb38973c03e53ac1b13680072
zhoutonghao <1789714191@qq.com>
2022-09-04 20:27:41 +0800

    #列表排序

--- python001/day06/双层for循环嵌套.py
+++ python001/day06/双层for循环嵌套.py
@@ -77,3 +77,20 @@ for i in range(len(list1)):
     for j in range(i + 1, len(list1)):
         if list1[i] == list1[j]:
             print(list1[i])
+"""
+对列表进行排序
+核心，将元素两两进行比较
+    发现更大的或者更小的规则进行交换
+        降序      升序
+"""
+
+for i in range(len(list1) - 1):
+    for j in range(i + 1, len(list1)):
+        if list1[i] < list1[j]:
+            temp = list1[j]
+            list1[j] = list1[i]
+            list1[i] = temp
+print(list1)
+
+# 做编程要保持一种开放的态度
+
